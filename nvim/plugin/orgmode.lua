5effa05479d80ca5c3343ab4fb893fc888c976d0
Ben-Heinze <ben.c.heinze@gmail.com>
2026-08-26 17:05:52 -0600

    changed auto-indent

--- nvim/plugin/orgmode.lua
+++ nvim/plugin/orgmode.lua
@@ -1,6 +1,10 @@
 require('orgmode').setup {
   org_agenda_files = '~/orgfiles/**/*',
   org_default_notes_file = '~/orgfiles/refile.org',
+  -- Don't auto-indent body content under headlines. Default `true` inserts real
+  -- spaces (heading level + 1) on every new line and stacks more under list
+  -- items, which makes typing lists tedious. Keep content flush left.
+  org_adapt_indentation = false,
 }
 
 -- Experimental LSP support
